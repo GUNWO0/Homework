b3df3a17abe6b1c100e0343aaba3923325860da3
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-04-13 18:03:35 +0900

    실습

---
+++ src/main/java/com/example/loginapp/board/BoardService.java
@@ -0,0 +1,23 @@
+package com.example.loginapp.board;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+
+    public List<Board> 글목록보기(Integer userId) {
+        if (userId == null) {
+            return boardRepository.findAll();
+        } else {
+            return boardRepository.findAll(userId);
+        }
+    }
+
+}
